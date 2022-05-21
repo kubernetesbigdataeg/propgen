11eed5933d5859a0ce997f16340860c96760ed7c
Javi Roman <jroman.espinar@gmail.com>
2022-05-21 08:50:22 +0200

    zookeeper test working

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,55 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package cmd
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+	"text/template"
+)
+
+const (
+	TEMPLATE int = 1
+	VALUES       = 2
+)
 
 func GenerateFile(label string, file string) {
-	fmt.Println(label)
-	fmt.Println(file)
+	p := make(map[string]string)
+	for _, e := range os.Environ() {
+		matched, _ := regexp.MatchString(fmt.Sprintf("^%s", label), e)
+		if matched {
+			subcad := strings.SplitN(e, "__", 3)
+			//fmt.Print(subcad[TEMPLATE])
+			//fmt.Println(subcad[VALUES])
+			p[strings.Replace(strings.SplitN(subcad[VALUES], "=", 2)[0], "_", ".", -1)] =
+				strings.SplitN(subcad[VALUES], "=", 2)[1]
+		}
+	}
+
+	fmt.Println(file, ":")
+	/*
+		coresite := "<?xml version=\"1.0\"?>\n" +
+			"<?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>\n" +
+			"<configuration>\n" +
+			"{{range $key, $val := .}}\n" +
+			"  <property>\n" +
+			"      <name>{{$key}}</name>\n" +
+			"      <value>{{$val}}</value>\n" +
+			"  </property>\n" +
+			"{{end}}\n" +
+			"</configuration>\n"
+
+	*/
+
+	zoocfg := "{{range $key, $val := .}}" +
+		"{{$key}}={{$val}}\n" +
+		"{{end}}"
+
+	tmp1, _ := template.New("zoocfg").Parse(zoocfg)
+	err := tmp1.Execute(os.Stdout, p)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
